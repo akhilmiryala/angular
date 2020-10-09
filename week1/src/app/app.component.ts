89c2452488ac9dc83f9aaed25911b7484c40dfcd
Laxman Maharjan <email@mlaxman.com.np>
2020-10-09 18:41:44 +0545

    week1

---
+++ week1/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'week1';
+
+  data: Array<{
+    name: string,
+    message: string
+  }> = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  login(form) {
+    const httpOpts = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
+    this.http.post('http://localhost:5200/_session', { }, httpOpts)
+      .subscribe();
+  }
+}
